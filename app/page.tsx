09a3440eb107b1c9c22eb2b5de906639861eeff6
Solar138 <46548002+solar138@users.noreply.github.com>
2026-05-30 19:48:37 -0700

    Add redirect from root page straight to courses/ucdavis, as that is the only page with useful tools right now.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/courses/ucdavis");
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans dark:bg-black">
       <main>
